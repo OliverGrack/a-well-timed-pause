57843d9be8fa170a766ef02a091dead4510d0e3c
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-10 18:39:21 +0200

    health display

---
+++ Assets/UI/SpriteFillIntValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteFillIntValue : MonoBehaviour
+{
+    public IntVar value;
+    public int maxValue;
+
+    private Image img;
+
+    private void Start() {
+        img = GetComponent<Image>();
+    }
+
+    private void Update() {
+        img.fillAmount = value.Value / (float)maxValue;
+    }
+}
