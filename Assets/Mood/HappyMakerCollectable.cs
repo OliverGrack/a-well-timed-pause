9fef840d56b6e5a64bf52ff8bcbba63194f49ca5
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-10 16:10:19 +0200

    world stuff

---
+++ Assets/Mood/HappyMakerCollectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HappyMakerCollectable : MonoBehaviour
+{
+    public TransformVar player;
+    public HappyStateData happyState;
+    public float happyTime;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.transform == player.Value) {
+            happyState.happyTime = Mathf.Max(happyState.happyTime, happyTime);
+        }
+    }
+}
