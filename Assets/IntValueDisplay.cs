8d452cc85dc6da00843cafcfe8a2c4bbfff95703
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-10 17:41:58 +0200

    ui

---
+++ Assets/IntValueDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class IntValueDisplay : MonoBehaviour
+{
+    private TMP_Text text;
+    public IntVar value;
+    public string formatter;
+    public string singularFormatter;
+
+    private int oldValue;
+
+    private void Start() {
+        text = GetComponent<TMP_Text>();
+        updateText();
+    }
+
+    private void Update() {
+        if (oldValue != value.Value) {
+            updateText();
+        }
+    }
+
+    private void updateText() {
+        oldValue = value.Value;
+        if (oldValue == 1) {
+            text.text = singularFormatter.Replace("{}", oldValue.ToString());
+        } else {
+            text.text = formatter.Replace("{}", oldValue.ToString());
+        }
+    }
+
+}
---
+++ Assets/VarLib/IntVar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntVar : ScriptableObject {
+    [SerializeField]
+    private int value;
+
+    public int Value { get => value; set => this.value = value; }
+
+    public bool hasDefaultValue;
+    public int defaultValue;
+
+    public void Reset() {
+        if (hasDefaultValue) {
+            value = defaultValue;
+        }
+    }
+}
+
