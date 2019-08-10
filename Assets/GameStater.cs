c4070cea64a7fa43f5836da616d9c8f321774899
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-10 19:33:21 +0200

    smooth happiness drawing circle

---
+++ Assets/GameStater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStater : MonoBehaviour
+{
+    private IEnumerator Start() {
+        yield return new WaitForSeconds(2);
+        while (true) {
+            if (Input.anyKey && Input.inputString != "") {
+                Debug.Log(Input.inputString);
+                SceneManager.LoadScene("WorldScene");
+            }
+            yield return null;
+        }
+    }
+}
