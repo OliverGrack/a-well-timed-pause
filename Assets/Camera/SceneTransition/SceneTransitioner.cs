a7a81ad7c3ebc600d00d1e32e1ddfd048e759e7b
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-10 21:12:49 +0200

    scene transitions

---
+++ Assets/Camera/SceneTransition/SceneTransitioner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    private string nextScene;
+    private Animator anim;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    public void StartTransitionTo(string scene) {
+        nextScene = scene;
+        anim.SetBool("Show", true);
+    }
+
+    public void _handleTransitionFinished() {
+        SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Assets/VarLib/IntResetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntResetter : MonoBehaviour
+{
+    public IntVar[] vars;
+
+    private void Start() {
+        foreach(var v in vars) {
+            v.Reset();
+        }
+    }
+}
