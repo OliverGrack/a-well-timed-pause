d39189de912479d0846372418f7d5fbe320cf96f
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-11 13:33:15 +0200

    enemy color change

--- Assets/Entities/Enemy/Enemy.cs
+++ Assets/Entities/Enemy/Enemy.cs
@@ -1,26 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
     public int health;
     public float size;
 
+    public Color hitColor;
+    public float recolorTime;
+
+    private SpriteRenderer[] renderers;
+    private Color[] oriColors;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        renderers = GetComponentsInChildren<SpriteRenderer>();
+        oriColors = renderers.Select(r => r.color).ToArray();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    IEnumerator recolorCoro() {
+        foreach(var r in renderers) {
+            r.color = hitColor;
+        }
+        yield return new WaitForSeconds(recolorTime);
+        for (int i=0; i<renderers.Length; i++) {
+            renderers[i].color = oriColors[i];
+        }
     }
 
-    public void ApplyDamage(int amount)
-    {
+    public void ApplyDamage(int amount) {
+        StopAllCoroutines();
+        StartCoroutine(recolorCoro());
+
+
         health -= amount;
         if(health < 0)
         {
