658cfd32032f740762d98077a8a91991c04ef536
Oliver Gstöttenbauer <Oliver.Gstoettenbauer@catalysts.cc>
2019-08-11 10:03:07 +0200

    web gl fullscreen

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+    public TransformVar player;
+    private TMP_Text text;
+    private Player
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+        text = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
